396e1a617155f32f9f6913060711931bf15ee98a
jagdish.saini <jagdish.saini@extramarks.net>
2022-02-06 15:44:08 +0530

    auto buy feature added

--- config/__init__.py
+++ config/__init__.py
@@ -6,12 +6,12 @@ load_dotenv()
 ENV_VAR = os.getenv("ENVIORNMENT")
 
 class LatokenSettings(BaseSettings):
-    apiKey: str = "25c0bf96-a2d4-4679-88b9-48c35efd3120"
-    apiSecret: str = b'MmFiZTJlY2YtOGM1Mi00NTA0LTlkYmItODljODA5M2NiMWJj'
+    apiKey: str = ""
+    apiSecret: str = b''
     
 class TwillioSettings(BaseSettings):
-    account_sid: str = "AC62b5f1f188b3dfe453b9acb713cf9e7a"
-    auth_token: str = "e4b27bf0cba4b64f9d40d2bfbdb07b95"
+    account_sid: str = ""
+    auth_token: str = ""
 
 class Settings(LatokenSettings,TwillioSettings):
     pass
